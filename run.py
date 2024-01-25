a2da4eb67d202d2e6c195c459d03fba40a74b789
Stepan214 <cimlerstepan@gmail.com>
2024-01-25 10:24:03 +0100

    interface switch to ipynb, example datasets

--- run.py
+++ run.py
@@ -1,18 +1,1 @@
-import os
-from app import data_imports
-from app import data_cleaning
-from app import summary_stats
-
-def main():
-    
-    filepath = input("Enter the filepath: ")
-
-    df = data_imports.read_file(filepath)
-
-    df = data_cleaning.clean(df)
-
-    summary_stats.summary_statistics(df)
-
-    # models TODO
-
-    # visualisations TODO
+# Flask app here in the future
