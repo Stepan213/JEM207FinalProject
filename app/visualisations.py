151de00ea9be5c6162c96a720b0a04805f2c01c9
Stepan214 <cimlerstepan@gmail.com>
2024-01-28 21:35:13 +0100

    added more visualisation methods

--- app/visualisations.py
+++ app/visualisations.py
@@ -8,12 +8,63 @@ class Visualization:
         self.variables = variables
         self.target = target
 
+    def plot_all(self):
+        self.plot_correlation()
+        self.plot_scatter()
+        self.plot_box()
+        self.plot_histogram()
+        self.plot_count()
+        self.plot_regression()
+        self.plot_kde()
+
     def plot_correlation(self):
-        corr = self.df[self.variables + [self.target]].corr()
+        plt.title("Correlation Heatmap")
+        corr = self.df[[self.target] + self.variables].corr()
         sns.heatmap(corr, annot=True, cmap='coolwarm')
         plt.show()
 
-    def plot_scatter(self):
+    def plot_scatter(self, x_var=None, y_var=None):
+        if not y_var:   
+            y_var = self.target
+
+        if x_var:
+            plt.title(f"Scatter Plot of {x_var} vs. {y_var}")
+            sns.scatterplot(x=x_var, y=y_var, data=self.df)
+            plt.show()
+        else:
+            plt.title("Pair Plot")
+            sns.pairplot(self.df[self.variables + [self.target]])
+            plt.show()
+
+    def plot_box(self, variable=None):
+        if not variable:
+            variable = self.target
+
+        plt.title(f"Box Plot of {variable}")
+        sns.boxplot(x=variable, data=self.df)
+        plt.show()
+
+    def plot_histogram(self):
         for var in self.variables:
-            sns.scatterplot(x=var, y=self.target, data=self.df)
+            plt.title(f"Histogram of {var}")
+            sns.histplot(self.df[var], kde=True)
+            plt.show()
+
+    def plot_count(self):
+        for var in self.variables:
+            if self.df[var].dtype == 'object' or self.df[var].nunique() < 10:
+                plt.title(f"Count Plot of {var}")
+                sns.countplot(x=var, hue=self.target, data=self.df)
+                plt.show()
+
+    def plot_regression(self):
+        for var in self.variables:
+            plt.title(f"Regression Plot of {var} vs. {self.target}")
+            sns.regplot(x=var, y=self.target, data=self.df)
+            plt.show()
+
+    def plot_kde(self):
+        for var in self.variables:
+            plt.title(f"KDE Plot of {var}")
+            sns.kdeplot(data=self.df, x=var)
             plt.show()
