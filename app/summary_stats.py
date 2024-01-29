96721d9ebe58eaed52dab69dbff2b7294abbb51c
Stepan214 <cimlerstepan@gmail.com>
2024-01-29 09:09:38 +0100

    added support for categorical variables

--- app/summary_stats.py
+++ app/summary_stats.py
@@ -1,43 +1,28 @@
 import pandas as pd
 
-def summary_statistics(df, description_interval="all", decimal_places=2):
+def numeric_statistics(df, decimal_places=2):
+    data = df.select_dtypes(include='number')
 
-    def get_stats(data):
-        stats = pd.DataFrame()
+    desc_stats = data.describe().round(decimal_places)
+    
+    additional_stats = pd.DataFrame(index=desc_stats.columns)
+    
+    additional_stats['mode'] = data.mode().iloc[0].round(decimal_places)
+    additional_stats['variance'] = data.var().round(decimal_places)
+    additional_stats['skewness'] = data.skew().round(decimal_places)
+    additional_stats['kurtosis'] = data.kurtosis().round(decimal_places)
+    additional_stats['missing_values'] = data.isnull().sum()
+    
+    return pd.concat([desc_stats.T, additional_stats], axis=1)
 
-        desc_stats = data.describe().round(decimal_places)
-        stats = pd.concat([stats, desc_stats.T], axis=1)
+def categorical_statistics(df):
+    data = df.select_dtypes(include='object')
 
-        mode_stats = data.mode().iloc[0].round(decimal_places)
-        mode_stats.name = 'mode'
-        stats = pd.concat([stats, mode_stats], axis=1)
+    stats = pd.DataFrame(index=data.columns)
 
-        var_stats = data.var().round(decimal_places)
-        var_stats.name = 'variance'
-        stats = pd.concat([stats, var_stats], axis=1)
+    stats['unique_values'] = data.nunique()
+    stats['mode'] = data.mode().iloc[0]
+    stats['frequency_of_mode'] = data.apply(lambda x: x.value_counts().iloc[0])
+    stats['missing_values'] = data.isnull().sum()
 
-        skew_stats = data.skew().round(decimal_places)
-        skew_stats.name = 'skewness'
-        stats = pd.concat([stats, skew_stats], axis=1)
-
-        kurtosis_stats = data.kurtosis().round(decimal_places)
-        kurtosis_stats.name = 'kurtosis'
-        stats = pd.concat([stats, kurtosis_stats], axis=1)
-
-        missing_values_stats = data.isnull().sum()
-        missing_values_stats.name = 'missing_values'
-        stats = pd.concat([stats, missing_values_stats], axis=1)
-
-        return stats
-
-    # Handling different description intervals
-    if description_interval == "all":
-        return get_stats(df)
-    elif description_interval == "none":
-        return pd.DataFrame()
-    else:
-        all_stats = {}
-        for interval in description_interval:
-            resampled_data = df.resample(interval).apply(lambda x: x.round(decimal_places))
-            all_stats[interval] = get_stats(resampled_data)
-        return all_stats
+    return stats
--- app/visualisations.py
+++ app/visualisations.py
@@ -1,71 +1,107 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+import pandas as pd
+import numpy as np
 
 class Visualization:
-    def __init__(self, df, variables, target):
+    def __init__(self, df, variables=None, target=None):
         self.df = df
-        self.variables = variables
-        self.target = target
+        self.target = target if target else self.df.columns.tolist()[-1]
+        self.variables = variables if variables else self.df.columns.tolist()
 
-        if not self.target:
-            self.target = self.df.columns.tolist()[-1]
-        if not self.variables:
-            self.variables = self.df.columns.tolist()
+        self.categorical_vars = []
+        self.numerical_vars = []
+        self.identify_variable_types()
+
+        if self.target in self.variables:
             self.variables.remove(self.target)
+
+    def identify_variable_types(self):
+        for var in self.variables:
+            if pd.api.types.is_numeric_dtype(self.df[var]):
+                self.numerical_vars.append(var)
+            else:
+                self.categorical_vars.append(var)
 
     def plot_all(self):
         self.plot_correlation()
-        self.plot_scatter()
-        self.plot_box()
-        self.plot_histogram()
-        self.plot_regression()
-        self.plot_kde()
+        self.plot_categorical()
+        for var in self.numerical_vars:
+            self.plot_scatter(x_var=var)
+            self.plot_box(variable=var)
+            self.plot_histogram(variable=var)
+            self.plot_regression(variable=var)
+            self.plot_kde(variable=var)
 
     def plot_correlation(self):
         plt.title("Correlation Heatmap")
-        corr = self.df[[self.target] + self.variables].corr()
+
+        # Filter only numeric columns for correlation
+        numeric_cols = self.df.select_dtypes(include=[np.number]).columns.tolist()
+
+        # Check if the target variable is numeric and include it if so
+        if pd.api.types.is_numeric_dtype(self.df[self.target]):
+            selected_cols = [self.target] + [col for col in numeric_cols if col != self.target]
+        else:
+            selected_cols = numeric_cols
+
+        corr = self.df[selected_cols].corr()
         sns.heatmap(corr, annot=True, cmap='coolwarm')
         plt.show()
 
+    def plot_categorical(self):
+        for var in self.categorical_vars:
+            plt.title(f"Count Plot of {var}")
+            sns.countplot(x=var, data=self.df)
+            plt.show()
+
     def plot_scatter(self, x_var=None, y_var=None):
-        if not y_var:   
+        if not y_var:
             y_var = self.target
 
         if x_var:
             plt.title(f"Scatter Plot of {x_var} vs. {y_var}")
-            sns.scatterplot(x=x_var, y=y_var, data=self.df)
+
+            if x_var in self.categorical_vars:
+                sns.swarmplot(x=x_var, y=y_var, data=self.df)
+            else:
+                sns.scatterplot(x=x_var, y=y_var, data=self.df)
+
             plt.show()
         else:
             sns.pairplot(self.df[self.variables + [self.target]])
             plt.show()
 
     def plot_box(self, variable=None):
+        if variable and variable in self.categorical_vars:
+            plt.title(f"Box Plot of {variable} by {self.target}")
+            sns.boxplot(x=variable, y=self.target, data=self.df)
+        elif variable and variable in self.numerical_vars:
+            plt.title(f"Box Plot of {variable}")
+            sns.boxplot(y=variable, data=self.df)
+        else:
+            plt.title(f"Box Plot of {self.target}")
+            sns.boxplot(y=self.target, data=self.df)
+        plt.show()
+
+    def plot_histogram(self, variable=None):
         if not variable:
             variable = self.target
 
-        plt.title(f"Box Plot of {variable}")
-        sns.boxplot(x=variable, data=self.df)
+        plt.title(f"Histogram of {variable}")
+        sns.histplot(self.df[variable], kde=True)
         plt.show()
 
-    def plot_histogram(self):
-        plt.title(f"Histogram of {self.target}")
-        sns.histplot(self.df[self.target], kde=True)
-        plt.show()
-
-        for var in self.variables:
-            plt.title(f"Histogram of {var}")
-            sns.histplot(self.df[var], kde=True)
+    def plot_regression(self, variable=None):
+        if not self.target in self.numerical_vars:
+            pass
+        elif variable and variable in self.numerical_vars:
+            plt.title(f"Regression Plot of {variable} vs. {self.target}")
+            sns.regplot(x=variable, y=self.target, data=self.df)
             plt.show()
 
-    def plot_regression(self):
-        for var in self.variables:
-            plt.title(f"Regression Plot of {var} vs. {self.target}")
-            sns.regplot(x=var, y=self.target, data=self.df)
-            plt.show()
-
-    def plot_kde(self):
-        for var in self.variables:
-            plt.title(f"KDE Plot of {var}")
-            sns.kdeplot(data=self.df, x=var)
+    def plot_kde(self, variable=None):
+        if variable and variable in self.numerical_vars:
+            plt.title(f"KDE Plot of {variable}")
+            sns.kdeplot(data=self.df, x=variable)
             plt.show()
